195bbd9c74af419aa2b763030932e786302ee566
Gussttaa <gusttavlim@hotmail.com>
2025-12-01 21:16:50 -0300

    Inicio por botões

--- projet_initial/lib/main.dart
+++ projet_initial/lib/main.dart
@@ -1,8 +1,48 @@
 import 'package:flutter/material.dart';
 
+class PerguntaApp extends StatelessWidget {
+  //const PerguntaApp({super.key});
 
+  var perguntaSelecionada = 0;
 
+  void responder() {
+    perguntaSelecionada++;
+    print('Pergunta respondida');
+  }
 
+  @override
+  Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sual cor favorita',
+      'Qual é o seu animal favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Perguntas')),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(onPressed: responder, child: Text('Resposta 1')),
+
+            ElevatedButton(
+              onPressed: responder,
+              /*() {print('Resposta 2 foi selecionada'); },*/
+              child: Text('Resposta 2'),
+            ),
+
+            ElevatedButton(
+              onPressed: responder,
+              /*() => print('Resposta 3'),*/
+              child: Text('Resposta 3'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+/*
 void main() {
   runApp(const MyApp());
 }
@@ -72,3 +112,4 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+*/
