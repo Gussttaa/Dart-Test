4aa844e0e16e40204bb6038039e8996d578bf56a
Gussttaa <gusttavlim@hotmail.com>
2025-12-04 21:38:33 -0300

    Push 2

--- projet_initial/lib/questoes.dart
+++ projet_initial/lib/questoes.dart
@@ -8,6 +8,7 @@ class Questao extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
+      margin: EdgeInsets.all(10),
       child: Text(
         Texto,
         style: TextStyle(fontSize: 28),
